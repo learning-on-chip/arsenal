adaf03174ba57b5813ccc9921944684e1e604a2d
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-10-14 15:57:01 +0200

    Refactor recorder.py

--- scripts/recorder.py
+++ scripts/recorder.py
@@ -2,101 +2,125 @@
 
 import os, sim, sys, time
 
-def die(message):
-    raise SystemError(message)
+class Config:
+    def __init__(self):
+        self.studio = os.getenv('STUDIO_ROOT')
+        if not self.studio: die('STUDIO_ROOT should be defined')
 
-studio = os.getenv('STUDIO_ROOT')
-if not studio: die('STUDIO_ROOT should be defined')
+        self.toolbox = os.getenv('TOOLBOX_ROOT')
+        if not self.toolbox: die('TOOLBOX_ROOT should be defined')
 
-toolbox = os.getenv('TOOLBOX_ROOT')
-if not toolbox: die('TOOLBOX_ROOT should be defined')
+        self.program = os.getenv('PROGRAM_NAME')
+        if not self.program: die('PROGRAM_NAME should be defined')
 
-program = os.getenv('PROGRAM_NAME')
-if not program: die('PROGRAM_NAME should be defined')
+        self.output = sim.config.output_dir
+        self.period = 1e6 * sim.util.Time.NS
 
-mcpat_bin = os.path.join(studio, 'scripts', 'mcpat.py')
-if not os.path.exists(mcpat_bin): die('cannot find mcpat.py')
+        self.redis = {
+            'bin': 'redis-cli',
+            'server': '127.0.0.1:6379',
+            'queue': 'recorder-%s' % self.program,
+        }
 
-recorder_bin = os.path.join(toolbox, 'bin', 'recorder')
-if not os.path.exists(recorder_bin): die('cannot find Recorder')
+        self.sqlite = {
+            'bin': 'sqlite3',
+            'database': os.path.join(self.output, '%s.sqlite3' % self.program),
+            'dynamic': 'dynamic',
+            'static': 'static',
+        }
 
-output = sim.config.output_dir
-period = 1e6 * sim.util.Time.NS
+class Worker:
+    def __init__(self, config):
+        self.program = config.program
+        self.output = config.output
+        self.redis = config.redis
+        self.sqlite = config.sqlite
 
-redis_bin = 'redis-cli'
-server = '127.0.0.1:6379'
-queue = 'recorder-%s' % program
+        self.recorder = os.path.join(config.toolbox, 'bin', 'recorder')
+        if not os.path.exists(self.recorder): die('cannot find Recorder')
 
-sqlite_bin = 'sqlite3'
-database = os.path.join(output, '%s.sqlite3' % program)
-dynamic_table = 'dynamic'
-static_table = 'static'
+        self.mcpat = os.path.join(config.studio, 'scripts', 'mcpat.py')
+        if not os.path.exists(self.mcpat): die('cannot find mcpat.py')
 
-log = os.path.join(output, '%s.log' % program)
-log = open(log, 'w')
+    def start_dynamic(self):
+        run('%s dynamic --server %s --queue %s --caching --database %s --table %s &' % (
+            self.recorder, self.redis['server'], self.redis['queue'],
+            self.sqlite['database'], self.sqlite['dynamic']
+        ))
 
-class Recorder:
-    def setup(self, args):
-        self.start_time = time.time()
-        reset()
+    def stop_dynamic(self):
+        run('%s RPUSH %s recorder:halt > /dev/null' % (
+            self.redis['bin'], self.redis['queue'])
+        )
+
+    def process_dynamic(self, t0, t1):
+        self.progress('%10.2f ms' % (t1 / sim.util.Time.MS))
+        filebase = os.path.join(self.output, 'dynamic-%s' % t0)
+        run('unset PYTHONHOME && %s -o %s -d %s --partial=%s:%s' % (
+            self.mcpat, filebase, self.output, t0, t1
+        ))
+        run('%s RPUSH %s "recorder:%.15e;%s" > /dev/null &' % (
+            self.redis['bin'], self.redis['queue'], float(t0) / sim.util.Time.S,
+            filebase + '.xml'
+        ))
+
+    def process_static(self, t):
+        filebase = os.path.join(self.output, 'static')
+        run('unset PYTHONHOME && %s -o %s -d %s --partial=%s:%s' % (
+            self.mcpat, filebase, self.output, 'start', t
+        ))
+        run('%s static --server %s --caching --config %s --database %s --table %s &' % (
+            self.recorder, self.redis['server'], filebase + '.xml',
+            self.sqlite['database'], self.sqlite['static']
+        ))
+
+    def reset(self):
+        run('%s DEL %s > /dev/null' % (self.redis['bin'], self.redis['queue']))
+        run('%s %s "DROP TABLE IF EXISTS %s;"' % (
+            self.sqlite['bin'], self.sqlite['database'], self.sqlite['static'])
+        )
+        run('%s %s "DROP TABLE IF EXISTS %s;"' % (
+            self.sqlite['bin'], self.sqlite['database'], self.sqlite['dynamic'])
+        )
+
+    def progress(self, message):
+        print('-------> [%-15s] %s' % (self.program, message))
+
+class Listener:
+    def __init__(self, worker, config):
+        self.worker = worker
+        self.period = config.period
+
+        filename = os.path.join(config.output, '%s.log' % config.program)
+        self.logger = open(filename, 'w')
+
+    def setup(self, _):
+        self.t_start = time.time()
         self.t_last = None
-        recorder_dynamic_start()
-        sim.util.Every(period, self.periodic, roi_only = True)
+        self.worker.start_dynamic()
+        sim.util.Every(self.period, self.periodic, roi_only=True)
 
     def periodic(self, t, _):
-        report('%10.2f ms' % (t / sim.util.Time.MS))
         sim.stats.write(str(t))
-        if self.t_last == None:
-            self.compute_static(t)
-        else:
-            self.compute_dynamic(self.t_last, t)
+        if self.t_last == None: self.worker.process_static(t)
+        else: self.worker.process_dynamic(self.t_last, t)
         self.t_last = t
 
     def hook_sim_end(self):
-        recorder_dynamic_stop()
-        log.write('Elapsed time: %s s\n' % (time.time() - self.start_time))
-
-    def compute_static(self, t):
-        filebase = os.path.join(output, 'static')
-        recorder_static_send(filebase, t)
-
-    def compute_dynamic(self, t0, t1):
-        filebase = os.path.join(output, 'dynamic-%s' % t0)
-        recorder_dynamic_send(filebase, t0, t1)
-
-def reset():
-    run('%s DEL %s > /dev/null' % (redis_bin, queue))
-    run('%s %s "DROP TABLE IF EXISTS %s;"' % (sqlite_bin, database, static_table))
-    run('%s %s "DROP TABLE IF EXISTS %s;"' % (sqlite_bin, database, dynamic_table))
-
-def recorder_dynamic_send(filebase, t0, t1):
-    run('unset PYTHONHOME && %s -o %s -d %s --partial=%s:%s' % (
-        mcpat_bin, filebase, output, t0, t1
-    ))
-    run('%s RPUSH %s "recorder:%.15e;%s" > /dev/null &' % (
-        redis_bin, queue, float(t0) / sim.util.Time.S, filebase + '.xml'
-    ))
-
-def recorder_dynamic_start():
-    run('%s dynamic --server %s --queue %s --caching --database %s --table %s &' % (
-        recorder_bin, server, queue, database, dynamic_table
-    ))
-
-def recorder_dynamic_stop():
-    run('%s RPUSH %s recorder:halt > /dev/null' % (redis_bin, queue))
-
-def recorder_static_send(filebase, t):
-    run('unset PYTHONHOME && %s -o %s -d %s --partial=%s:%s' % (
-        mcpat_bin, filebase, output, 'start', t
-    ))
-    run('%s static --server %s --caching --config %s --database %s --table %s &' % (
-        recorder_bin, server, filebase + '.xml', database, static_table
-    ))
-
-def report(message):
-    print('-------> [%-15s] %s' % (program, message))
+        self.worker.stop_dynamic()
+        self.logger.write('Elapsed time: %s s\n' % (time.time() - self.t_start))
 
 def run(command):
     if os.system(command) != 0: die('failed to run `%s`' % command)
 
-sim.util.register(Recorder())
+def die(message):
+    raise SystemError(message)
+
+config = Config()
+
+worker = Worker(config)
+worker.reset()
+
+listener = Listener(worker, config)
+
+sim.util.register(listener)
