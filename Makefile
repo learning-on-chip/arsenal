5639a9bd02a1a318e905785d73fde61ad66a3fe7
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-10-25 08:55:24 +0200

    Make a cosmetic adjustment

--- Makefile
+++ Makefile
@@ -1,23 +1,23 @@
 export STUDIO_ROOT := $(shell pwd)
-export TOOLBOX_ROOT := $(STUDIO_ROOT)/toolbox
+export TOOLBOX_ROOT := ${STUDIO_ROOT}/toolbox
 
 ifndef OUTPUT_ROOT
-export OUTPUT_ROOT := $(STUDIO_ROOT)/result
+export OUTPUT_ROOT := ${STUDIO_ROOT}/result
 endif
 
 all:
 	@echo 'Usage: make record-<suite name>-<benchmark name>-<input size>'
 
 install update:
-	@$(MAKE) -C toolbox $@
+	@${MAKE} -C toolbox $@
 
 setup:
 	@redis-server config/redis.conf
 
 record-%:
-	@$(MAKE) -C program $*
+	@${MAKE} -C program $*
 
 clean kill:
-	@$(MAKE) -C program $@
+	@${MAKE} -C program $@
 
 .PHONY: all clean install kill setup update
--- program/suite.mk
+++ program/suite.mk
@@ -24,50 +24,50 @@ options += -s ${STUDIO_ROOT}/script/recorder.py
 
 define declare_benchmark
 ifdef OUTPUT_DIR
-$(2)_output := ${OUTPUT_DIR}
+${2}_output := ${OUTPUT_DIR}
 else
-$(2)_output := ${OUTPUT_ROOT}/$(1)/$(2)
+${2}_output := ${OUTPUT_ROOT}/${1}/${2}
 endif
 
-$(2)-%: $${$(2)_output}/.$(2)-%
+${2}-%: $${${2}_output}/.${2}-%
 	@exit 0
 
-$${$(2)_output}/.$(2)-%:
+$${${2}_output}/.${2}-%:
 	@(                                                                       \
-	    program="$(1)-$(2)-$$*";                                             \
-	    options="-d $${$(2)_output} $$$$(./configure.py $$$${program})";     \
-	    mkdir -p $${$(2)_output};                                            \
-	    cd $${$(2)_output};                                                  \
+	    program="${1}-${2}-$$*";                                             \
+	    options="-d $${${2}_output} $$$$(./configure.py $$$${program})";     \
+	    mkdir -p $${${2}_output};                                            \
+	    cd $${${2}_output};                                                  \
 	    echo "Start recording $$$${program}...";                             \
 	    PROGRAM_NAME=$$$${program} $${SNIPER_BIN} $${options} $$$${options}; \
 	    echo "Finish recording $$$${program}."                               \
 	)
 	@touch $$@
 
-$(2)-clean:
-	@rm -rf $${$(2)_output}
+${2}-clean:
+	@rm -rf $${${2}_output}
 
-$(2)-kill:
-	@killall -q -KILL -- $(2) || true
+${2}-kill:
+	@killall -q -KILL -- ${2} || true
 
-.PHONY: $(2)-clean $(2)-kill
-.PRECIOUS: $${$(2)_output}/.$(2)-%
+.PHONY: ${2}-clean ${2}-kill
+.PRECIOUS: $${${2}_output}/.${2}-%
 endef
 
 define declare_suite
 ifdef INPUT_SIZE
-all: $(addsuffix -${INPUT_SIZE},$(2))
+all: $(addsuffix -${INPUT_SIZE},${2})
 else
 all:
 	@echo "INPUT_SIZE should be defined"
 	@exit 1
 endif
 
-$(foreach benchmark,$(2),$(eval $(call declare_benchmark,$(1),$(benchmark))))
+$(foreach benchmark,${2},$(eval $(call declare_benchmark,${1},${benchmark})))
 
-clean: $(addsuffix -clean,$(2))
+clean: $(addsuffix -clean,${2})
 
-kill: $(addsuffix -kill,$(2))
+kill: $(addsuffix -kill,${2})
 
 .PHONY: all clean kill
 endef
